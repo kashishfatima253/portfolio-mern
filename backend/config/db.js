a4251c7441242b0c7ec4755bcdca1cfa1cc86c8c
Kashish Fatima <kashishfatima253@gmail.com>
2023-06-23 01:22:55 +0500

    created server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGO_URI = "mongodb+srv://test_user:wpCP3OTsToIWD6ii@cluster0.kjrkiv7.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = process.env.MONGO_URI
 const connectDB = () => {
     mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
